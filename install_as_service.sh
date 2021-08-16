58627718edcaf9485d018f3f236de0d32ce79655
LDLDL <hyx1278199910@outlook.com>
2021-08-16 17:05:05 +0800

    Update install_as_service.sh

--- install_as_service.sh
+++ install_as_service.sh
@@ -4,6 +4,7 @@ mkdir /etc/cfddns
 cp cfddns.py /etc/cfddns/
 cp conf.json /etc/cfddns/
 cp cfddns.service /usr/lib/systemd/system/
+touch /etc/cfddns/cfddns.log
 systemctl daemon-reload
 systemctl enable cfddns
 systemctl start cfddns
