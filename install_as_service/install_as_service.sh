1d12f70768c0b418cb4a0c8ed22a55cd17aa7723
桜庭清夏 <SakurabaKiyoka@yahoo.co.jp>
2023-02-14 16:08:32 +0900

    install as service

---
+++ install_as_service/install_as_service.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+
+if ! grep -q 'systemd' /proc/1/comm
+then
+    echo 'systemd not found.'
+    exit 1
+fi
+
+
+mkdir -p /opt/cfddns
+python3 ./config.py
+cp cfddns.py /opt/cfddns/
+cp -r sources /opt/cfddns/
+cp cfddns.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable cfddns
+systemctl start cfddns
